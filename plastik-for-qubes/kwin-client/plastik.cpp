147fb0dd94f9205a2abd78f91aeebd3b0ed0edd1
Marek Marczykowski <marmarek@invisiblethingslab.com>
2013-02-07 01:39:39 +0100

    plastic-for-qubes: export API version

--- plastik-for-qubes/kwin-client/plastik.cpp
+++ plastik-for-qubes/kwin-client/plastik.cpp
@@ -550,11 +550,16 @@ PlastikHandler* Handler()
 // Plugin Stuff                                                             //
 //////////////////////////////////////////////////////////////////////////////
 
+
+// KWIN_DECORATION(KWinPlastik::PlastikHandler)
 extern "C"
 {
     KDE_EXPORT KDecorationFactory *create_factory()
     {
         KWinPlastik::handler = new KWinPlastik::PlastikHandler();
         return KWinPlastik::handler;
+    }
+    KWIN_EXPORT int decoration_version() {
+        return KWIN_DECORATION_API_VERSION;
     }
 }

5b9a5bedb91e19c681b1112ffb82fd46ebb67b6a
Marta Marczykowska-Górecka <marmarta@invisiblethingslab.com>
2023-07-04 14:59:08 +0200

    Update config script for newer KDE and Qubes Menu
    Requires https://github.com/QubesOS/qubes-desktop-linux-menu/pull/34

--- 10-qubes.js
+++ 10-qubes.js
@@ -1,20 +1,22 @@
+/* replace the default kickoff with Qubes menu */
 var panels = panels()
 for (var i in panelIds) {
 	var panel = panelById(panelIds[i])
 	var widgetIds = panel.widgetIds
 	for (id in widgetIds) {
 		var widget = panel.widgetById(widgetIds[id])
-		if (widget.type == 'launcher') {
-			var simpleMenu = panel.addWidget('simplelauncher')
-			simpleMenu.index = widget.index + 1
-			simpleMenu.writeConfig('icon', 'start-here-qubes')
+		if (widget.type == 'org.kde.plasma.kickoff') {
+			var qubesMenu = panel.addWidget('org.kde.plasma.quicklaunch')
+			qubesMenu.index = widget.index
+			qubesMenu.currentConfigGroup = ['General']
+			qubesMenu.writeConfig('launcherUrls', ['file:///usr/share/applications/open-qubes-app-menu.desktop'])
 			widget.remove()
 		}
 	}
 }
 
 /* wallpaper */
-var activity = activities()[0];
-activity.currentConfigGroup = new Array('Wallpaper', 'image');
-activity.writeConfig('wallpaper', '/usr/share/wallpapers/Qubes_Steel');
-activity.writeConfig('wallpaperposition', '2');
+var desktop = desktops()[0];
+desktop.wallpaperPlugin = "org.kde.image"
+desktop.currentConfigGroup = Array('Wallpaper', 'org.kde.image', "General");
+desktop.writeConfig('Image', 'file:///usr/share/wallpapers/Qubes_Steel');
